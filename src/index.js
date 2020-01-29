32b9facfabf374009da0c7d04b66c9edc6b09305
giuliana-enea <60190300+giuliana-enea@users.noreply.github.com>
2020-01-29 16:11:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-@import 'carbon-components/scss/globals/scss/styles.scss';
 import 'core-js/modules/es7.array.includes';
 import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
